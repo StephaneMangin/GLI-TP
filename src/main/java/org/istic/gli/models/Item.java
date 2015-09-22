2578b33abdecd9d09f5da20a042274d8610a0c1b
lucas <lucas.galton@gmail.com>
2015-09-22 17:18:05 +0200

    Item structure creation

---
+++ src/main/java/org/istic/gli/models/Item.java
@@ -0,0 +1,34 @@
+package org.istic.gli.models;
+
+/**
+ * Created by lucas on 22/09/15.
+ */
+public class Item {
+    private String title;
+    private String desc;
+    private Float value;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Float getValue() {
+        return value;
+    }
+
+    public void setValue(Float value) {
+        this.value = value;
+    }
+}
--- src/main/java/org/istic/gli/models/Model.java
+++ src/main/java/org/istic/gli/models/Model.java
@@ -7,36 +7,6 @@ package org.istic.gli.models;
 public class Model
 {
 
-	private double amount;
-	private String comment;
-	private String name;
-	
-	public Model(){
-		super();
-	}
 
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
 
